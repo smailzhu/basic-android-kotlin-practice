04c5ce95206c2dedce963b139ec0a5bd9b8d43ee
smailzhu <smailzhu.tw+git@gmail.com>
2022-06-18 23:58:54 +0800

    Finish lecture Fragments and the Navigation component use navigation to control the fragment
    Finish lecture Fragments and the Navigation component
    use navigation to control the fragment

--- android-basics-kotlin-words-app-starter/app/src/main/java/com/example/wordsapp/LetterAdapter.kt
+++ android-basics-kotlin-words-app-starter/app/src/main/java/com/example/wordsapp/LetterAdapter.kt
@@ -23,6 +23,7 @@ import android.view.ViewGroup
 import android.view.accessibility.AccessibilityNodeInfo
 import android.widget.Button
 import androidx.annotation.RequiresApi
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 
 /**
@@ -64,11 +65,11 @@ class LetterAdapter :
         val item = list.get(position)
         holder.button.text = item.toString()
 
-        val context = holder.view.context
-        val intent = Intent(context, DetailActivity::class.java)
+//        val context = holder.view.context
+//        val intent = Intent(context, DetailActivity::class.java)
         holder.button.setOnClickListener {
-            intent.putExtra(DetailActivity.LETTER, holder.button.text.toString())
-            context.startActivity(intent)
+            val action = LetterListFragmentDirections.actionLetterListFragmentToWordListFragment(letter = holder.button.text.toString())
+            holder.view.findNavController().navigate(action)
         }
     }
 
---
+++ android-basics-kotlin-words-app-starter/app/src/main/java/com/example/wordsapp/WordListFragment.kt
@@ -0,0 +1,61 @@
+package com.example.wordsapp
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.wordsapp.databinding.FragmentLetterListBinding
+
+class WordListFragment : Fragment() {
+    private lateinit var letterId: String
+
+    companion object {
+        const val LETTER = "letter"
+        const val SEARCH_PREFIX = "https://www.google.com/search?q="
+    }
+
+    private var _binding: FragmentLetterListBinding? = null // ? represent nullable variable
+    private val binding get() = _binding!! // get() means this property is "get-only"
+    private lateinit var recyclerView: RecyclerView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        arguments?.let {
+            letterId = it.getString(LETTER).toString()
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentLetterListBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        recyclerView = binding.recyclerView
+
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        recyclerView.adapter = WordAdapter(letterId, requireContext())
+
+        // Adds a [DividerItemDecoration] between items
+        recyclerView.addItemDecoration(
+            DividerItemDecoration(context, DividerItemDecoration.VERTICAL)
+        )
+
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
