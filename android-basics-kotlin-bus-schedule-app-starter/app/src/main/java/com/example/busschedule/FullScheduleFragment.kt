83325a1b3640859c11920931949ad8def335caaf
smailzhu <smailzhu.tw+git@gmail.com>
2022-06-24 00:56:19 +0800

    Finish Introduction to Room and Flow

--- android-basics-kotlin-bus-schedule-app-starter/app/src/main/java/com/example/busschedule/FullScheduleFragment.kt
+++ android-basics-kotlin-bus-schedule-app-starter/app/src/main/java/com/example/busschedule/FullScheduleFragment.kt
@@ -21,6 +21,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.coroutineScope
+import kotlinx.coroutines.flow.collect
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -31,7 +33,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
-class FullScheduleFragment: Fragment() {
+class FullScheduleFragment : Fragment() {
 
     private var _binding: FullScheduleFragmentBinding? = null
 
@@ -67,8 +69,11 @@ class FullScheduleFragment: Fragment() {
         // call from potentially locking the UI, you should use a
         // coroutine scope to launch the function. Using GlobalScope is not
         // best practice, and in the next step we'll see how to improve this.
-        GlobalScope.launch(Dispatchers.IO) {
-            busStopAdapter.submitList(viewModel.fullSchedule())
+
+        lifecycle.coroutineScope.launch {
+            viewModel.fullSchedule().collect() {
+                busStopAdapter.submitList(it)
+            }
         }
     }
 
--- android-basics-kotlin-bus-schedule-app-starter/app/src/main/java/com/example/busschedule/StopScheduleFragment.kt
+++ android-basics-kotlin-bus-schedule-app-starter/app/src/main/java/com/example/busschedule/StopScheduleFragment.kt
@@ -21,7 +21,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.coroutineScope
 import androidx.navigation.findNavController
+import kotlinx.coroutines.flow.collect
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.busschedule.databinding.StopScheduleFragmentBinding
@@ -74,8 +76,10 @@ class StopScheduleFragment: Fragment() {
         // call from potentially locking the UI, you should use a
         // coroutine scope to launch the function. Using GlobalScope is not
         // best practice, and in the next step we'll see how to improve this.
-        GlobalScope.launch(Dispatchers.IO) {
-            busStopAdapter.submitList(viewModel.scheduleForStopName(stopName))
+        lifecycle.coroutineScope.launch {
+            viewModel.scheduleForStopName(stopName).collect() {
+                busStopAdapter.submitList(it)
+            }
         }
     }
 
