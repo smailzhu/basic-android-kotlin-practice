48fa99ecddbd2ec153efb9faae6c9ee30447c071
smailzhu <smailzhu.tw+git@gmail.com>
2022-06-23 19:14:16 +0800

    Finish Amphibians app

--- android-basics-kotlin-amphibians-app-main/app/src/main/java/com/example/amphibians/ui/AmphibianViewModel.kt
+++ android-basics-kotlin-amphibians-app-main/app/src/main/java/com/example/amphibians/ui/AmphibianViewModel.kt
@@ -15,24 +15,56 @@
  */
 package com.example.amphibians.ui
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.amphibians.network.Amphibian
+import com.example.amphibians.network.AmphibianApi
+import kotlinx.coroutines.launch
 
-enum class AmphibianApiStatus {LOADING, ERROR, DONE}
+enum class AmphibianApiStatus { LOADING, ERROR, DONE }
 
 class AmphibianViewModel : ViewModel() {
 
     // TODO: Create properties to represent MutableLiveData and LiveData for the API status
+    private val _status = MutableLiveData<AmphibianApiStatus>()
+    val status: LiveData<AmphibianApiStatus> = _status
 
     // TODO: Create properties to represent MutableLiveData and LiveData for a list of amphibian objects
+    private val _amphibians = MutableLiveData<List<Amphibian>>()
+    val amphibians: LiveData<List<Amphibian>> = _amphibians
 
     // TODO: Create properties to represent MutableLiveData and LiveData for a single amphibian object.
     //  This will be used to display the details of an amphibian when a list item is clicked
+    private val _amphibian = MutableLiveData<Amphibian>()
+    val amphibian: MutableLiveData<Amphibian> = _amphibian
 
     // TODO: Create a function that gets a list of amphibians from the api service and sets the
     //  status via a Coroutine
 
     fun onAmphibianClicked(amphibian: Amphibian) {
         // TODO: Set the amphibian object
+        _amphibian.value = amphibian
+    }
+
+    init {
+        getAmphibianList()
+    }
+
+    private fun getAmphibianList(){
+        viewModelScope.launch {
+            _status.value = AmphibianApiStatus.LOADING
+            try {
+                _amphibians.value = AmphibianApi.retrofitService.getAmphibians()
+                _status.value = AmphibianApiStatus.DONE
+            } catch (e: Exception) {
+                Log.d("viewmodel", e.toString())
+                _status.value = AmphibianApiStatus.ERROR
+                _amphibians.value = listOf()
+            }
+        }
+
     }
 }
