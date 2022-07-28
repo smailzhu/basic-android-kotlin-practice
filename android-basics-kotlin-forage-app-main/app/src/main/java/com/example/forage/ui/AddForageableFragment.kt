3dbed27abff60ea4cfbfdff155d3342144404ed3
smailzhu <smailzhu.tw+git@gmail.com>
2022-07-28 18:06:05 +0800

    Complete Forage project

--- android-basics-kotlin-forage-app-main/app/src/main/java/com/example/forage/ui/AddForageableFragment.kt
+++ android-basics-kotlin-forage-app-main/app/src/main/java/com/example/forage/ui/AddForageableFragment.kt
@@ -24,10 +24,12 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import com.example.forage.BaseApplication
 import com.example.forage.R
 import com.example.forage.databinding.FragmentAddForageableBinding
 import com.example.forage.model.Forageable
 import com.example.forage.ui.viewmodel.ForageableViewModel
+import com.example.forage.ui.viewmodel.ForageableViewModelFactory
 
 
 /**
@@ -46,10 +48,14 @@ class AddForageableFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
-    // TODO: Refactor the creation of the view model to take an instance of
+    // Refactor the creation of the view model to take an instance of
     //  ForageableViewModelFactory. The factory should take an instance of the Database retrieved
     //  from BaseApplication
-    private val viewModel: ForageableViewModel by activityViewModels()
+    private val viewModel: ForageableViewModel by activityViewModels() {
+        ForageableViewModelFactory(
+            (activity?.application as BaseApplication).database.foragableDao()
+        )
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -66,8 +72,12 @@ class AddForageableFragment : Fragment() {
         val id = navigationArgs.id
         if (id > 0) {
 
-            // TODO: Observe a Forageable that is retrieved by id, set the forageable variable,
+            // Observe a Forageable that is retrieved by id, set the forageable variable,
             //  and call the bindForageable method
+            viewModel.retrieveForageable(id).observe(this.viewLifecycleOwner) { selectedItem ->
+                forageable = selectedItem
+                bindForageable(forageable)
+            }
 
             binding.deleteBtn.visibility = View.VISIBLE
             binding.deleteBtn.setOnClickListener {
--- android-basics-kotlin-forage-app-main/app/src/main/java/com/example/forage/ui/viewmodel/ForageableViewModel.kt
+++ android-basics-kotlin-forage-app-main/app/src/main/java/com/example/forage/ui/viewmodel/ForageableViewModel.kt
@@ -15,28 +15,32 @@
  */
 package com.example.forage.ui.viewmodel
 
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.*
 import com.example.forage.data.ForageableDao
 import com.example.forage.model.Forageable
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import java.lang.IllegalArgumentException
 
 /**
  * Shared [ViewModel] to provide data to the [ForageableListFragment], [ForageableDetailFragment],
  * and [AddForageableFragment] and allow for interaction the the [ForageableDao]
  */
 
-// TODO: pass a ForageableDao value as a parameter to the view model constructor
+// pass a ForageableDao value as a parameter to the view model constructor
 class ForageableViewModel(
     // Pass dao here
+    private val forageableDao: ForageableDao
 ): ViewModel() {
 
-    // TODO: create a property to set to a list of all forageables from the DAO
+    // create a property to set to a list of all forageables from the DAO
+    val allForageables: LiveData<List<Forageable>> = forageableDao.getForageables().asLiveData()
 
-    // TODO : create method that takes id: Long as a parameter and retrieve a Forageable from the
+    // create method that takes id: Long as a parameter and retrieve a Forageable from the
     //  database by id via the DAO.
+    fun retrieveForageable(id: Long): LiveData<Forageable>{
+        return forageableDao.getForageable(id).asLiveData()
+    }
 
     fun addForageable(
         name: String,
@@ -51,8 +55,8 @@ class ForageableViewModel(
             notes = notes
         )
 
-    // TODO: launch a coroutine and call the DAO method to add a Forageable to the database within it
-
+    // launch a coroutine and call the DAO method to add a Forageable to the database within it
+        viewModelScope.launch { forageableDao.insert(forageable) }
     }
 
     fun updateForageable(
@@ -70,13 +74,15 @@ class ForageableViewModel(
             notes = notes
         )
         viewModelScope.launch(Dispatchers.IO) {
-            // TODO: call the DAO method to update a forageable to the database here
+            // call the DAO method to update a forageable to the database here
+            forageableDao.update(forageable)
         }
     }
 
     fun deleteForageable(forageable: Forageable) {
         viewModelScope.launch(Dispatchers.IO) {
-            // TODO: call the DAO method to delete a forageable to the database here
+            // call the DAO method to delete a forageable to the database here
+            forageableDao.delete(forageable)
         }
     }
 
@@ -85,5 +91,16 @@ class ForageableViewModel(
     }
 }
 
-// TODO: create a view model factory that takes a ForageableDao as a property and
+//  create a view model factory that takes a ForageableDao as a property and
 //  creates a ForageableViewModel
+class ForageableViewModelFactory(private val forageableDao: ForageableDao): ViewModelProvider.Factory{
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+//        Check if the modelClass is the same as the ForageableViewModel class and return an instance of it. Otherwise, throw an exception.
+        if(modelClass.isAssignableFrom(ForageableViewModel::class.java)){
+            @Suppress("UNCHECK_CAST")
+            return ForageableViewModel(forageableDao) as T
+        }
+        throw IllegalArgumentException("Unknow ViewModel class")
+    }
+
+}
