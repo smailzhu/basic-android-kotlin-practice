00c3d746754847f1e842c6dcb8bf4dcc748c382c
smailzhu <smailzhu.tw+git@gmail.com>
2022-08-04 16:18:37 +0800

    Finish water!me codelab

--- android-basics-kotlin-water-me-app-main/app/src/main/java/com/example/waterme/viewmodel/PlantViewModel.kt
+++ android-basics-kotlin-water-me-app-main/app/src/main/java/com/example/waterme/viewmodel/PlantViewModel.kt
@@ -18,11 +18,14 @@ package com.example.waterme.viewmodel
 import android.app.Application
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import androidx.work.WorkManager
+import androidx.work.*
 import com.example.waterme.data.DataSource
+import com.example.waterme.worker.WaterReminderWorker
+import com.example.waterme.worker.WaterReminderWorker.Companion.nameKey
 import java.util.concurrent.TimeUnit
 
 class PlantViewModel(application: Application): ViewModel() {
+    private val workManager = WorkManager.getInstance(application)
 
     val plants = DataSource.plants
 
@@ -32,11 +35,26 @@ class PlantViewModel(application: Application): ViewModel() {
         plantName: String
     ) {
         // TODO: create a Data instance with the plantName passed to it
+        val data  = Data.Builder()
+        data.putString(nameKey, plantName)
 
         // TODO: Generate a OneTimeWorkRequest with the passed in duration, time unit, and data
         //  instance
+        var continuation = workManager.beginUniqueWork(
+            nameKey,
+            ExistingWorkPolicy.REPLACE,
+            OneTimeWorkRequest.from(WaterReminderWorker::class.java)
+        )
+
+        val remindRequest = OneTimeWorkRequestBuilder<WaterReminderWorker>()
+            .setInputData(data.build())
+            .build()
+
+        continuation = continuation.then(remindRequest)
 
         // TODO: Enqueue the request as a unique work request
+        continuation.enqueue()
+
     }
 }
 
